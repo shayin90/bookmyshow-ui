90dd0aee2366192f95542f81254043777e84fafb
Shayin <shayinbanu29@gmail.com>
2024-09-25 03:35:39 +0530

    added view movie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
+import { ViewMovieComponent } from './view-movie/view-movie.component';
 
 export const routes: Routes = [
     {path:'home', component: HomeComponent},
     {path: 'movies', component:MoviesListComponent},
+    {path:'movies/:id',component: ViewMovieComponent},
     {path:'', redirectTo :'/home', pathMatch :'full'}
 ];
---
+++ src/app/view-movie/view-movie.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-view-movie',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './view-movie.component.html',
+  styles: ``
+})
+export class ViewMovieComponent {
+  movieName!: string;
+  movie:any;
+
+  constructor(private route:ActivatedRoute){
+this.movieName=this.route.snapshot.params["id"];
+this.movie = this.movies.find(obj=> obj.Title ==this.movieName);
+  }
+  movies = [
+    {
+      Title:"Meiyazhagan",
+      imgUrl:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/meiyazhagan-et00405427-1721627107.jpg",
+      description: "comedy/Drama",
+        },
+        {
+          Title:"Nandan",
+          imgUrl:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/nandhan-et00410955-1725626073.jpg",
+          description: "comedy/Drama",
+            },
+            {
+              Title:"Chelladurai",
+              imgUrl:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/kozhipannai-chelladurai-et00411870-1726560826.jpg",
+              description: "comedy/Drama",
+                },
+                {
+                  Title:"Devara",
+                  imgUrl:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/devara--part-1-et00310216-1712750637.jpg",
+                  description: "comedy/Drama",
+                    },
+                    {
+                      Title:"Tambbad",
+                      imgUrl:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/tumbbad-et00079092-1726221741.jpg",
+                      description: "Horror/comedy/Drama",
+                        },
+                        {
+                          Title:"Kishkindha-kaandam",
+                          imgUrl:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/kishkindha-kaandam-et00410024-1726051976.jpg",
+                          description: "Horror/comedy/Drama",
+                            },
+                            {
+                              Title:"Vazhai",
+                              imgUrl:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/vaazhai-et00408594-1724233930.jpg",
+                              description: "Horror/comedy/Drama",
+                                },
+                                {
+                                  Title:"Demonte-colony",
+                                  imgUrl:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/demonte-colony-2--vengeance-of-the-unholy-et00380018-1723644092.jpg",
+                                  description: "Horror/comedy/Drama",
+                                    },
+   ]
+
+}
