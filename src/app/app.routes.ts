fb22c95e288ce61f28a43d8401ee9a30eadc1975
Shayin <shayinbanu29@gmail.com>
2024-09-25 02:54:48 +0530

    added movies-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
 export const routes: Routes = [
     {path:'home', component: HomeComponent},
+    {path: 'movies', component:MoviesListComponent},
     {path:'', redirectTo :'/home', pathMatch :'full'}
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MoviesListComponent } from "../movies-list/movies-list.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MoviesListComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
